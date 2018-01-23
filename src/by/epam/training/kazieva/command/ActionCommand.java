f4469360f38373a13df99ef3bf08024e91a16a33
kazieva <32552023+kazieva@users.noreply.github.com>
2018-01-23 17:54:48 +0200

    Revert "logination"
    This reverts commit 38455fc4c0b0ba6fdba4eeebf9bd298fba7766fc.

--- src/by/epam/training/kazieva/command/ActionCommand.java
+++
@@ -1,8 +0,0 @@
-package by.epam.training.kazieva.command;
-
-import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
-
-public interface ActionCommand {
-    String execute(HttpServletRequest request) throws SQLException, ClassNotFoundException;
-}
