1a32f232e641a61c6504c68b9adacc50c89f503b
kazieva <32552023+kazieva@users.noreply.github.com>
2018-01-26 14:48:39 +0200

    filter

---
+++ src/by/epam/training/kazieva/entity/Abiturient.java
@@ -0,0 +1,7 @@
+package by.epam.training.kazieva.entity;
+
+/**
+ * Created by YiaKrevetko on 26.01.2018.
+ */
+public class Abiturient {
+}
---
+++ src/by/epam/training/kazieva/filter/EncodingFilter.java
@@ -0,0 +1,34 @@
+package by.epam.training.kazieva.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"},
+        initParams = {
+                @WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding Param")})
+public class EncodingFilter implements Filter {
+    private String code;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        code = filterConfig.getInitParameter("encoding");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String codeRequest = request.getCharacterEncoding();
+        // установка кодировки из параметров фильтра, если не установлена
+        if (code != null && !code.equalsIgnoreCase(codeRequest)) {
+            request.setCharacterEncoding(code);
+            response.setCharacterEncoding(code);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        code = null;
+    }
+}
